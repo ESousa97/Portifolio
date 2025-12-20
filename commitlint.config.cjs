697886578c69b4de177d4b0165b0280b900a3fc7
Sousa <xfuuuuuuuuury2018@gmail.com>
2025-12-20 17:08:23 -0300

    ci: allow dependabot commits

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,3 +1,5 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
+  // Ignora commits automáticos do Dependabot (ex.: "Bump pacote from x to y")
+  ignores: [(message) => message.startsWith("Bump ") || message.startsWith("chore(deps)")],
 };
